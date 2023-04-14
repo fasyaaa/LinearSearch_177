68327148b4fd9eb0ff911346f4149734be5e15f1
fasyaaa <muhammad.a.f.2210@gmail.com>
2023-04-14 10:49:28 +0700

    8

--- LinearSearch/LinearSearch.cpp
+++ LinearSearch/LinearSearch.cpp
@@ -36,15 +36,15 @@ void LinearSearch()
 	do
 	{
 		// Accept the number to be searched
-		cout << "\nEnter the element you want to search : ";				//Langkah 1
+		cout << "\nEnter the element you want to search : ";				// Langkah 1
 		int item;
 		cin >> item;
 
 		comparison = 0;
-		for (i = 0; i < n; i++)
+		for (i = 0; i < n; i++)												// Langkah 2,3 dan 4
 		{
 			comparison++;
-				if (arr[i] == item)
+				if (arr[i] == item)											// Langkah 5 a found
 				{
 					cout << "\n" << item << " found at position" << (i + 1) << endl;
 					break;
