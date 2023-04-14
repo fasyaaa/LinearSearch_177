5dbafa783b306f75d3b338f42e981b15e9eb780b
fasyaaa <muhammad.a.f.2210@gmail.com>
2023-04-14 10:33:11 +0700

    1

---
+++ LinearSearch/LinearSearch.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int arr[20];
+int n;
+int i;
+
+void input()
+{
+	while (true)
+	{
+		cout << "Enter the number of element in the array : ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimum 1 and maximum 20 element.\n\n";
+	}
+
+
+}
